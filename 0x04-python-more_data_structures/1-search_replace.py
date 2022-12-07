4b33ba693c70eeca663c9dc5ee6cc6bd9bf2fc13
ella7d <ella.debesai@gmail.com>
2022-12-07 09:21:08 -0800

    1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return (my_list(map(lambda x: x.replace('search', 'replace'))))
+    return(my_list(map(lambda x: x.replace('search', 'replace'))))

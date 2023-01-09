db8e1339975e76d008991cde5e1e51aca1265e62
ella7d <ella.debesai@gmail.com>
2023-01-09 07:23:47 -0800

    geometry

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 """contains the MyList class"""
- class MyList(list):
-     """a subclass of list"""
-     def__init__(self):
-         """initializes the object"""
-         super().__init__()
 
-         def paint_sorted(self):
+
+class MyList(list):
+    """a subclass of list"""
+
+    def print_sorted(self):
              """prints sorted list"""
-             print(sorted(self)
+        print(sorted(self)

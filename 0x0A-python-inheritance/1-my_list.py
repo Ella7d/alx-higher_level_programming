903bcc7eaa936ea6841a63cfee1d2da4be15cbed
ella7d <ella.debesai@gmail.com>
2023-01-09 03:24:57 -0800

    1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""contains the MyList class"""
+ class MyList(list):
+     """a subclass of list"""
+     def__init__(self):
+         """initializes the object"""
+         super().__init__()
+
+         def paint_sorted(self):
+             """prints sorted list"""
+             print(sorted(self)

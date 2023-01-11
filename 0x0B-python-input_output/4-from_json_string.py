87a1cd5bcf65028eadd2f1053f6ade3ea7804c4d
ella7d <ella.debesai@gmail.com>
2023-01-11 12:02:23 -0800

    fix

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Write a function that returns an object /"""
+"""
+contains the json str function
+"""
+
 import json
 
 
 def from_json_string(my_str):
-        """ Write a function that returns an object (Python data structure) represented by a JSON string:"""
-         return json.loads(my_str)
-                   
+    """returns an object represented by a JSON string"""
+    return json.loads(my_str)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Write a script that adds all arguments to a Python list, and then save
-   them to a file:
-        *If the file doesnt exist, it should be created
 """
+Script that adds all arguments to a Python list, and then saves them to a file
+"""
+
 from sys import argv
-from os import path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
+
 try:
-    my_items = load_from_json_file(filename)
-
+    json_list = load_from_json_file(filename)
 except FileNotFoundError:
-    my_items = []
-for i in range(1, len(argv)):
-    my_items.append(argv[i])
+    json_list = []
 
-save_to_json_file(my_items, filename)
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json_file(json_list, filename)
